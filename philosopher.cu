7a4dbadb60c4e37fff609a8b3234842eac502104
JosephDelaneJanicki <47838316+JosephDelaneJanicki@users.noreply.github.com>
2023-09-14 12:49:54 -0500

    added device functions to fork object

--- philosopher.cu
+++ philosopher.cu
@@ -9,6 +9,7 @@ public:
 
     // TODO: Define philosopher states and actions 
 
+
     //Due to Philosopher being an object its best to handle device memory with device functions made below
     void allocateToDevice(Philosopher** devicePtr){
 
@@ -21,27 +22,38 @@ public:
         cudaFree(this);
     }
 };
-// Fork object to allow solutions that involve changing something about the fork/forks.
+// Fork object to allow solutions that involve changing something about the fork/forks. Forks are picked up and put down by philosophers
 class Fork{
 public:
     int temprature {}; // how hot or cold the fork is to the touch
-    bool available = true;  //fork availability to pick up
-    
+    bool available = true;  //fork availability to pick up, by default they are available
+    // this method allows the fork to be picked up
+    bool pickUp(){
+        
+        if (available) available = false;
+        else return false;
+
+    }
+    // this method allows the fork to be put down
+    void putDown() {available = true;}
 };
 
 // kernel definition
 //in a way we can think about this as the table itself
-__global__ void philosopherSimulation(Philosopher* philosophers, int numPhilosophers) {
-    //maps each philosopher to one thread
-    int philosopherIdx = threadIdx.x;
+__global__ void philosopherSimulation(Philosopher* philosophers, Fork* forks, int numPhilosophers) {
+    //thread mapping
+    int philosopherIdx = blockIdx.x; // one block per philosopher
+    int forkIdx = threadIdx.x; // one thread per fork
     
     // TODO: Simulate philosopher actions (picking up forks, eating, thinking, ect)
 }
 
 // this is a testing kernal I will call apon when debugging.
 // the idea is this is a single solution table of 5 philosophers I can use to test and garuntee any one solution no matter how absurd does infact, end deadlock
-__global__ void testDeadlockResolution(Philosopher* philosophers, Fork* forks, int numPhilosophers){
-    int philosopherIdx = threadIdx.x;
+__global__ void testDeadlockResolution(Philosopher* philosophers, Fork* forks, int numPhilosophers = 5){
+    int philosopherIdx = blockIdx.x; 
+    int forkIdx = threadIdx.x;
+    
     
 }
 
